9d686f4c104f0b21ff7139df6f504c2c26f8858f
Chih-Chen Kao <elros.morlin@gmail.com>
2018-06-28 16:21:47 +0800

    prevent cmd from closing after the program finished

--- excel_statistics/main.py
+++ excel_statistics/main.py
@@ -877,5 +877,14 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except:
+        print(sys.exc_info()[0])
+        import traceback
+        print(traceback.format_exc())
+    finally:
+        print("Press Enter to continue ...") 
+        input()
+
 
