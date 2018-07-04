6d0903b493122b88b40bc80e383e7f21c0a5fab2
Chih-Chen Kao <elros.morlin@gmail.com>
2018-07-04 17:48:09 +0800

    add color to output

--- excel_statistics/click_me_first.py
+++ excel_statistics/click_me_first.py
@@ -1,13 +1,14 @@
 
 
-import sys
-import subprocess
+
 
 try:
-    subprocess.call(['pip', 'install', "pandas", "xlrd", "openpyxl"])
+    import subprocess
+    subprocess.call(['pip', 'install', "pandas", "xlrd", "openpyxl", "colorama"])
     print("Installing modules ... ")
 
 except:
+    import sys
     print(sys.exc_info()[0])
     import traceback
     print(traceback.format_exc())
