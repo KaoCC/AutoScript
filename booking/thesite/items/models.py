6dcdd5567a0eb6db212e53c76a66570ec9713311
Chih-Chen Kao <elros.morlin@gmail.com>
2019-01-21 20:29:38 +0800

    add str function

--- booking/thesite/items/models.py
+++ booking/thesite/items/models.py
@@ -9,6 +9,10 @@ class Item(models.Model):
     # serial_number = models.CharField(max_length=300)
     # lend_from = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        """return item name"""
+        return self.name
+
 
 import uuid
 from datetime import date
@@ -18,7 +22,7 @@ class ItemInstance(models.Model):
     serial_number = models.CharField(max_length=300)
     item = models.ForeignKey('Item', on_delete = models.SET_NULL, null=True)
     due_back = models.DateField(null=True, blank=True)
-    
+
     # borrower
 
 
@@ -27,3 +31,9 @@ class ItemInstance(models.Model):
         if self.due_back and date.today() > self.due_back:
             return True
         return False
+
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return "{0} ({1} : {2})".format(self.id, self.serial_number, self.item.name)
+
